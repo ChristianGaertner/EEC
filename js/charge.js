797b9f308c7952670bbd4b9bf24028e69404b002
Christian Gärtner <chrisdagardner@gmail.com>
2015-06-08 18:36:44 +0200

    Capacitor charge calculator works, units are not yet

--- js/charge.js
+++ js/charge.js
@@ -1,31 +1,34 @@
+var RAW_GETTER_REGEX = /([0-9]*(.|,)[0-9]*)/i;
+
 var ChargeCalc = React.createClass({
     displayName: 'ChargeCalc',
     getInitialState: function () {
         return {
-            value1: "200 V",
-            value2: "200 pF",
-            value3: "40 nF",
-            value4: "4 µJ"
+            value1: "1 V",
+            value2: "1 F",
+            value3: "1 C",
+            value4: "0.5 J"
         };
     },
-    updateUI: function(digit, node) {
+    updateUI: function(digit, value) {
         document
             .getElementById('svg_charge')
             .getSVGDocument()
             .getElementById('display_value' + digit)
-            .textContent = node.options[node.selectedIndex].text;
+            .textContent = value;
     },
     handleChange: function() {
-        var v1 = React.findDOMNode(this.refs.value1);
-        var v2 = React.findDOMNode(this.refs.value2);
-        var v3 = React.findDOMNode(this.refs.value3);
-        var v4 = React.findDOMNode(this.refs.value4);
+        var v1 = React.findDOMNode(this.refs.value1).value;
+        var v2 = React.findDOMNode(this.refs.value2).value;
+
+        var v3 = this.getCharge(v1, v2);
+        var v4 = this.getEnergy(v1, v2);
 
         this.setState({
-            value1: v1.value,
-            value2: v2.value,
-            value3: v3.value,
-            value4: v4.value
+            value1: v1,
+            value2: v2,
+            value3: v3,
+            value4: v4
         });
 
         // updateUI
@@ -34,15 +37,37 @@ var ChargeCalc = React.createClass({
         this.updateUI('3', v3);
         this.updateUI('4', v4);
     },
+    getRaw: function(v) {
+        if (!v | v == "") {
+            return 0;
+        }
+        return parseFloat(v.match(RAW_GETTER_REGEX)[0]);
+    },
+    getCharge: function(u, c) {
+        var q = this.getRaw(u) * this.getRaw(c);
+        if (isNaN(q)) {
+            return "--";
+        }
+        return  q + " C";
+    },
+    getEnergy: function(u, c) {
+        var rawU = this.getRaw(u);
+        var e = 0.5 * this.getRaw(c) * rawU * rawU;
+        console.log(e);
+        if (isNaN(e)) {
+            return "--";
+        }
+        return e + " J";
+    },
     render: function () {
         return (
             <div>
-                <h5>Cooming soon!</h5>
+                <h5>ONLY USE VOLTS AND FARADS AS UNITS!</h5>
                 Voltage&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(U):
-                &nbsp;<input ref="value1" type="text" placeholder="200V" />
+                &nbsp;<input ref="value1" onChange={ this.handleChange } type="text" value={ this.state.value1 } />
                 <br />
                 Capacitance&nbsp;(C):
-                &nbsp;<input ref="value2" type="text" placeholder="200pF"/>
+                &nbsp;<input ref="value2" onChange={ this.handleChange } type="text" value={ this.state.value2 }/>
             </div>
         )
     }
